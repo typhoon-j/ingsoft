961c5e74887da30aa9d3843c0753635869ba89f9
fabriariex-22 <fabricio.crespo.requena@gmail.com>
2020-11-09 21:13:12 -0400

    Solucionando error

--- src/main/java/bo/ucb/edu/ingsoft/bl/MakeOrderBl.java
+++ src/main/java/bo/ucb/edu/ingsoft/bl/MakeOrderBl.java
@@ -17,37 +17,9 @@ public class MakeOrderBl {
     private TransactionDao transactionDao;
 
     @Autowired
-    public MakeOrderBl(ProductAddDao productAddDao, TransactionDao transactionDao){
+    public MakeOrderBl(ProductAddDao productAddDao, TransactionDao transactionDao) {
         this.productAddDao = productAddDao;
         this.transactionDao = transactionDao;
 
-    }
-    public MakeOrder createProduct(MakeOrder makeOrder, Transaction transaction){
-        product.setBrand(productAdd.getBrand());
-        product.setModel(productAdd.getModel());
-        product.setPrice(productAdd.getPrice());
-        product.setDescription(productAdd.getDescription());
-        product.setStock(productAdd.getStock());
-        product.setTxId(transaction.getTxId());
-        product.setTxUserId(transaction.getTxUserId());
-        product.setTxHost(transaction.getTxHost());
-        product.setTxDate(transaction.getTxDate());
-        productAddDao.create(product);
-        Integer getLastId = transactionDao.getLastInsertId();
-        product.setProductId(getLastId);
-
-        hProduct.setProductId(product.getProductId());
-        hProduct.setBrand(productAdd.getBrand());
-        hProduct.setModel(productAdd.getModel());
-        hProduct.setPrice(productAdd.getPrice());
-        hProduct.setDescription(productAdd.getDescription());
-        hProduct.setStock(productAdd.getStock());
-        hProduct.setTxId(transaction.getTxId());
-        hProduct.setTxUserId(transaction.getTxUserId());
-        hProduct.setTxHost(transaction.getTxHost());
-        hProduct.setTxDate(transaction.getTxDate());
-        hProductDao.createHProduct(hProduct);
-
-        return productAdd;
     }
 }
